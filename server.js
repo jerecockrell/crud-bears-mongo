52879033fd35ca40b01be835235823cd5f49ec65
jerecockrell <jere.cockrell@gmail.com>
2016-02-16 14:06:15 -0700

    add new bear function

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/animals');
 
+var Bear = require('./models/bear');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -12,9 +14,43 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next){
+	console.log('something is happening!');
+	next();
+});
+
 router.get('/', function(req, res) {
 	res.json({ title : 'hooray it worked!!' });
 });
+
+
+
+router.route('/bears').post(function(req, res){
+
+		var bear = new Bear();
+		
+		bear.name = req.body.name;
+		bear.age = req.body.age;
+		bear.gender = req.body.gender;
+		
+		bear.save(function(err, bear){
+			if(err){
+				console.log(err)//do something
+			} else {
+				res.json(bear)//do something
+			}
+		})
+
+	})
+	.get(function(req, res){
+
+		res.json({title: "I am trying to GET"})
+
+	});
+	
+
+
+router.get('/bear')
 app.use('/api', router);
 app.listen(port, function(){
 	console.log("app listening on port " + port)
