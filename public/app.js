099937d9980fcd9e3852ecbf2d76994b48549c4b
jerecockrell <jere.cockrell@gmail.com>
2016-02-18 12:08:12 -0700

    add delete bear method using AJAX, etc.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,27 @@
-$('.deleteBear').on('click', function() {
-	alert('You deleted me!');
-});
+var deleteBear = function() {
+	
+	var id = $(event.target).closest('tr').attr('id');
+	var bear = $(event.target).closest('tr');
+
+	if(confirm('Are you sure you want to delete this bear?')){
+		$.ajax({
+			url: 'api/bears/' + id,
+			method: 'DELETE',
+		}).done(function() {
+			console.log('bear deleted!');
+			bear.remove();
+		})
+	}
+}
+
+$('.deleteBear').on('click', deleteBear);
+event.preventDefault();
+
+var addBear = function(){
+	var name = $('#name').val();
+	var age = $('#age').val();
+	var gender = $('#gender').val();
+	alert(name)
+}
+
+$('.addBear').on('click', addBear);
